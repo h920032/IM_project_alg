aa3525073036ba4e4e30d6bf8eb4aca668af33cb
dipsy <h920032@gmail.com>
2019-12-13 01:52:02 +0800

    modify

--- data/fixed/ARRANGEMENT.py
+++ data/fixed/ARRANGEMENT.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 import random as rd
 
-def ARRANGEMENT(W):
-    for i in range(W.shape[0]):
-        for j in range(W.shape[1]):
-            if W[i][j].sum() == 0:
-                W[i][j][rd.randint(1,W.shape[2])-1] = 1
-    return W
+def ARRANGEMENT(work, nEMPLOYEE, nDAY, nK):
+    for i in range(nEMPLOYEE):
+        for j in range(nDAY):
+            is_arrange = False
+            for k in range(nK):
+                if work[i,j,k] == True:
+                    is_arrange = True
+            if is_arrange == False:
+                work[i,j,rd.randint(1,nK)-1] = True
+    return work
