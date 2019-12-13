3e5b7f04f1064723de8a309b7b6e65f7607034c3
dipsy <h920032@gmail.com>
2019-12-13 21:59:33 +0800

    gene

--- data/fixed/ARRANGEMENT.py
+++ data/fixed/ARRANGEMENT.py
@@ -3,12 +3,18 @@ import pandas as pd
 import random as rd
 
 def ARRANGEMENT(work, nEMPLOYEE, nDAY, nK):
+    fix = []
     for i in range(nEMPLOYEE):
+        employee = []
         for j in range(nDAY):
             is_arrange = False
             for k in range(nK):
                 if work[i,j,k] == True:
                     is_arrange = True
+                    employee.append(1)
             if is_arrange == False:
-                work[i,j,rd.randint(1,nK)-1] = True
-    return work
+                rand = rd.randint(1,nK)
+                work[i,j,rand-1] = True
+                employee.append(0)
+        fix.append(employee)
+    return work, fix
