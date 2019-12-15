489c45a61a7e9af2ebc9899e627694455520e215
Ting0-0 <48677950+Ting0-0@users.noreply.github.com>
2019-12-16 00:48:15 +0800

    修正

--- data/fixed/gene_alg.py
+++ data/fixed/gene_alg.py
@@ -14,24 +14,26 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month):
     for i in range(len(score_liz)):
         for j in range(len(score_liz)):
             if i != j:
+                print('基因演算法： i =',i,', j =',j)
                 union = np.logical_or(score_liz[i][1], score_liz[j][1])
                 one_not_avb = union * score_liz[i][0]
                 one_avb = score_liz[i][0] - one_not_avb
                 two_not_avb = union * score_liz[j][0]
                 two_avb = score_liz[j][0] - two_not_avb
+                #隨機決定切分點
                 sp_row = random.randint(0,nDAY-1)
                 sp_col = random.randint(0,nEMPLOYEE-1)
+                #第一組：依據員工、日期切分
                 one_col_left = one_avb[:sp_col]
                 one_col_right = one_avb[sp_col:]
-                # one_row_up = one_avb[:,:sp_row]
-                # one_row_down = one_avb[:,sp_row:]
                 one_row_up = one_avb.T[:sp_row].T
                 one_row_down = one_avb.T[sp_row:].T
-                print('check point 1')
+                #第二組：的切分
                 two_col_left = two_avb[:sp_col]
                 two_col_right = two_avb[sp_col:]
                 two_row_up = two_avb.T[:sp_row].T
                 two_row_down = two_avb.T[sp_row:].T
+                #將對應的一、二組片段重新組合
                 a_one_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + one_not_avb
                 a_two_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + two_not_avb
                 a_one_two_one = np.concatenate((two_row_up, one_row_down), axis=1) + one_not_avb
