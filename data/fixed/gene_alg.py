a3b5e1872f2e6e0e57684882ded63bc7be4fed6d
dipsy <h920032@gmail.com>
2019-12-16 00:41:06 +0800

    gene

--- data/fixed/gene_alg.py
+++ data/fixed/gene_alg.py
@@ -9,11 +9,12 @@ K_type_dict = {1:'O',2:'A2',3:'A3',4:'A4',5:'A5',6:'MS',7:'AS',8:'P2',9:'P3',10:
 #def score(input):
 #    return random.randint(1,10000)
 
-def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month):
-    sort = sorted(score_liz, key = lambda s: s[2],reverse = True)
-    for i in range(len(score_liz)):
+def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month): #演算法部分
+    sort = sorted(score_liz, key = lambda s: s[2],reverse = True) #先按照分數排序
+    for i in range(len(score_liz)): #從中兩兩抓取互相配對
         for j in range(len(score_liz)):
             if i != j:
+                print(i)
                 union = np.logical_or(score_liz[i][1], score_liz[j][1])
                 one_not_avb = union * score_liz[i][0]
                 one_avb = score_liz[i][0] - one_not_avb
@@ -23,15 +24,12 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month):
                 sp_col = random.randint(0,nEMPLOYEE-1)
                 one_col_left = one_avb[:sp_col]
                 one_col_right = one_avb[sp_col:]
-                # one_row_up = one_avb[:,:sp_row]
-                # one_row_down = one_avb[:,sp_row:]
-                one_row_up = one_avb.T[:sp_row].T
-                one_row_down = one_avb.T[sp_row:].T
-                print('check point 1')
+                one_row_up = one_avb[:,:sp_row]     #!!??
+                one_row_down = one_avb[:,sp_row:]
                 two_col_left = two_avb[:sp_col]
                 two_col_right = two_avb[sp_col:]
-                two_row_up = two_avb.T[:sp_row].T
-                two_row_down = two_avb.T[sp_row:].T
+                two_row_up = two_avb[:,:sp_row]
+                two_row_down = two_avb[:,sp_row:]
                 a_one_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + one_not_avb
                 a_two_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + two_not_avb
                 a_one_two_one = np.concatenate((two_row_up, one_row_down), axis=1) + one_not_avb
@@ -40,7 +38,7 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month):
                 b_two_one_two = np.concatenate((one_col_left, two_col_right), axis=0) + two_not_avb
                 b_one_two_one = np.concatenate((two_col_left, one_col_right), axis=0) + one_not_avb
                 b_two_two_one = np.concatenate((two_col_left, one_col_right), axis=0) + two_not_avb
-                sort.append((a_one_one_two,score_liz[i][1],score(a_one_one_two.tolist(),nDAY,nW,year=year,month=month)))#!!??
+                sort.append((a_one_one_two,score_liz[i][1],score(a_one_one_two.tolist(),nDAY,nW,year=year,month=month)))
                 sort.append((a_two_one_two,score_liz[j][1],score(a_two_one_two.tolist(),nDAY,nW,year=year,month=month)))
                 sort.append((a_one_two_one,score_liz[i][1],score(a_one_two_one.tolist(),nDAY,nW,year=year,month=month)))
                 sort.append((a_two_two_one,score_liz[j][1],score(a_two_two_one.tolist(),nDAY,nW,year=year,month=month)))
@@ -48,8 +46,8 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month):
                 sort.append((b_two_one_two,score_liz[j][1],score(b_two_one_two.tolist(),nDAY,nW,year=year,month=month)))
                 sort.append((b_one_two_one,score_liz[i][1],score(b_one_two_one.tolist(),nDAY,nW,year=year,month=month)))
                 sort.append((b_two_two_one,score_liz[j][1],score(b_two_two_one.tolist(),nDAY,nW,year=year,month=month)))
-    sort = sorted(sort, key = lambda s: s[2],reverse = True)
-    sort = sort[:100]
+    sort = sorted(sort, key = lambda s: s[2],reverse = True) #交配完之後排序
+    sort = sort[:len(score_liz)] #取出前一百名
     return sort
 
 def gene_alg(avaliable_sol, fix, nDAY,nW, nEMPLOYEE, gen,year,month): #avaliavle_sol 可行解列表 fix 不能移動的列表
