1e20c8f56c0ae0f92133a5ce9ca367be2c4c1cc6
dipsy <h920032@gmail.com>
2019-12-18 23:33:47 +0800

    gene

--- data/fixed/gene_alg.py
+++ data/fixed/gene_alg.py
@@ -30,8 +30,8 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month,per_month_dir='./data/per_month
                 one_avb = new[i][0] - one_not_avb
                 two_not_avb = union * new[j][0]
                 two_avb = new[j][0] - two_not_avb
-                one_org = new[i][0]
-                two_org = new[j][0]
+                one_org = new[i][0] #沒有fix的班表
+                two_org = new[j][0] 
                 #隨機決定切分點
                 sp_row = random.randint(0,nDAY-1)
                 sp_col = random.randint(0,nEMPLOYEE-1)
@@ -40,20 +40,36 @@ def alg(score_liz, nDAY,nW, nEMPLOYEE,year,month,per_month_dir='./data/per_month
                 one_col_right = one_avb[sp_col:]
                 one_row_up = one_avb.T[:sp_row].T
                 one_row_down = one_avb.T[sp_row:].T
+                one_org_col_left = one_org[:sp_col]
+                one_org_col_right = one_org[sp_col:]
+                one_org_row_up = one_org.T[:sp_row].T
+                one_org_row_down = one_org.T[:sp_row].T
                 #第二組：的切分
                 two_col_left = two_avb[:sp_col]
                 two_col_right = two_avb[sp_col:]
                 two_row_up = two_avb.T[:sp_row].T
                 two_row_down = two_avb.T[sp_row:].T
+                two_org_col_left = two_org[:sp_col]
+                two_org_col_right = two_org[sp_col:]
+                two_org_row_up = two_org.T[:sp_row].T
+                two_org_row_down = two_org.T[:sp_row].T
+ 
                 #將對應的一、二組片段重新組合
+
+                #上下黏合
                 a_one_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + one_not_avb
                 a_two_one_two = np.concatenate((one_row_up, two_row_down), axis=1) + two_not_avb
                 a_one_two_one = np.concatenate((two_row_up, one_row_down), axis=1) + one_not_avb
                 a_two_two_one = np.concatenate((two_row_up, one_row_down), axis=1) + two_not_avb
+
+
+                #左右黏合
                 b_one_one_two = np.concatenate((one_col_left, two_col_right), axis=0) + one_not_avb
                 b_two_one_two = np.concatenate((one_col_left, two_col_right), axis=0) + two_not_avb
                 b_one_two_one = np.concatenate((two_col_left, one_col_right), axis=0) + one_not_avb
                 b_two_two_one = np.concatenate((two_col_left, one_col_right), axis=0) + two_not_avb
+
+
                 sort.append((a_one_one_two,score_liz[i][1],score(a_one_one_two.tolist(),nDAY,nW,year=year,month=month,per_month_dir=per_month_dir,AssignTest=AssignTest,NeedTest=NeedTest,EmployeeTest=EmployeeTest)))
                 sort.append((a_two_one_two,score_liz[j][1],score(a_two_one_two.tolist(),nDAY,nW,year=year,month=month,per_month_dir=per_month_dir,AssignTest=AssignTest,NeedTest=NeedTest,EmployeeTest=EmployeeTest)))
                 sort.append((a_one_two_one,score_liz[i][1],score(a_one_two_one.tolist(),nDAY,nW,year=year,month=month,per_month_dir=per_month_dir,AssignTest=AssignTest,NeedTest=NeedTest,EmployeeTest=EmployeeTest)))
